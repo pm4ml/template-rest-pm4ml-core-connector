f9620a80016eee3615771102892c7df5ef05ffb2
Yordan Stoykov <yordan.stoykov@modusbox.com>
2020-06-17 18:37:49 +0300

    Changed GET /parties to include responses from get loan and get client

--- client-adapter/src/main/java/com/modusbox/client/router/PartiesRouter.java
+++ client-adapter/src/main/java/com/modusbox/client/router/PartiesRouter.java
@@ -6,6 +6,7 @@ import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.cxf.common.message.CxfConstants;
 
 import java.util.Base64;
+import java.util.LinkedList;
 
 public class PartiesRouter extends RouteBuilder {
 
@@ -16,15 +17,41 @@ public class PartiesRouter extends RouteBuilder {
 
     public void configure() {
 
-    	// In this case the GET parties will return the loan account
-        from("direct:getParties")
-	        .log("Account lookup API called (loan account)")
-			.log("GET on /${header.idType}/${header.idValue} called")
-//	        .setProperty("inboundIdType", header("idType"))
-//	        .setProperty("inboundIdValue", header("idValue"))
+//			 Using Camel authentication query parameters
+//			.toD(host + "/clients/${header.accountId}" +
+//				"?authMethod=Basic&authUsername=" + username + "&authPassword=" + password); //.to() doesn't work here
 
+//	        .setHeader(CxfConstants.OPERATION_NAME, constant("AccountLookup"))
+//	        .to("direct:callT24EndPoint")
+
+//			.unmarshal().json()
+
+		// In this case the GET parties will return the loan account with client details
+		from("direct:getParties")
+			.log("Account lookup API called")
+			.log("GET on /${header.idType}/${header.idValue} called")
+			// First fetch the loan account by ID
+			.to("direct:getLoanById")
+			.process(new Processor() {
+				public void process(Exchange exchange) throws Exception {
+					System.out.println();
+				}
+			})
+			// Now fetch the client information for the user the loan acc belongs to
+			.to("direct:getClientById")
+			.process(new Processor() {
+				public void process(Exchange exchange) throws Exception {
+					System.out.println();
+				}
+			})
+			// Format the 2 responses
+			.bean("getPartiesResponse")
+        ;
+
+		// API call to Mambu for loan information by ID
+		from("direct:getLoanById")
+			.log("Get loan account by ID route called")
 			.setBody(simple("{}"))
-//			.bean("getPartiesRequest")
 
 			.removeHeaders("CamelHttp*")
 			.setHeader("Content-Type", constant("application/json"))
@@ -32,27 +59,18 @@ public class PartiesRouter extends RouteBuilder {
 			.setHeader(Exchange.HTTP_METHOD, constant("GET"))
 			.setHeader("Authorization", constant(authorizationHeader))
 			.toD(host + "/loans/${header.idValue}")
-			// Using Camel authentication query parameters
-//			.toD(host + "/clients/${header.accountId}" +
-//				"?authMethod=Basic&authUsername=" + username + "&authPassword=" + password); //.to() doesn't work here
 
-//	        .setHeader(CxfConstants.OPERATION_NAME, constant("AccountLookup"))
-//	        .to("direct:callT24EndPoint")
+			.unmarshal().json()
+			.setProperty("getLoanByIdResponse", body())
+			.setHeader("getLoanByIdResponse", body())
 
-			// Breakpoint processor
-//			.unmarshal().json()
-			.process(new Processor() {
-				public void process(Exchange exchange) throws Exception {
-					System.out.println();
-				}
-			})
-	        .bean("getPartiesResponse")
+//			.bean("getLoanByIdResponse")
 		;
 
 
 		// API call to Mambu for client information by ID
 		from("direct:getClientById")
-			.log("Get Client by ID route called")
+			.log("Get client by ID route called")
 			.setBody(simple("{}"))
 
 			.removeHeaders("CamelHttp*")
@@ -60,8 +78,13 @@ public class PartiesRouter extends RouteBuilder {
 			.setHeader("Accept", constant("application/json"))
 			.setHeader(Exchange.HTTP_METHOD, constant("GET"))
 			.setHeader("Authorization", constant(authorizationHeader))
-			.toD(host + "/clients/${header.idValue}")
+			.toD(host + "/clients/" + simple("${exchangeProperty.getLoanByIdResponse?.get('accountHolderKey')}").getText())
+//			.toD(host + "/clients/" + exchange.getProperty("getLoanByIdResponse").get("accountHolderKey"))
+//			.toD(host + "/clients/" + exchangeProperty("getLoanByIdResponse"))
 
+			.unmarshal().json()
+			.setProperty("getClientByIdResponse", body())
+			.setHeader("getClientByIdResponse", body())
 //			.bean("getClientByIdResponse")
 		;
 
