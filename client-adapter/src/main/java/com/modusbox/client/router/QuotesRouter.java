afabddf1c9cfc8743445e86e54a60b5fc720f2a4
Yordan Stoykov <yordan.stoykov@modusbox.com>
2020-06-25 11:55:14 +0300

    Added mock quote request response, change DOB format by get parties, changed the spring tomcat port to 9090

--- client-adapter/src/main/java/com/modusbox/client/router/QuotesRouter.java
+++ client-adapter/src/main/java/com/modusbox/client/router/QuotesRouter.java
@@ -9,13 +9,17 @@ public class QuotesRouter extends RouteBuilder {
 
         from("direct:postQuoterequests")
 			.log("POST Quotes API called")
-//			.setProperty("origPayload", simple("${body}"))
+			.setProperty("origPayload", simple("${body}"))
+
+			.setHeader(Exchange.HTTP_RESPONSE_CODE, constant(200))
+			.setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
+			.bean("postQuoterequestsResponseMock")
 
 //			.bean("postQuoterequestsRequest")
 
 //			.setBody(simple("${exchangeProperty.origPayload}")) //Need to know how this exchange property can be accessed in datasonnet file.
-			.setHeader(Exchange.HTTP_RESPONSE_CODE, constant(501))
-			.setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
+//			.setHeader(Exchange.HTTP_RESPONSE_CODE, constant(501))
+//			.setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
 //			.setBody(constant("NOT IMPLEMENTED"))
 
 //          .bean("postQuoterequestsResponse")
