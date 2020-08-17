4b568fec748836f0002dd5050ee7ee59144e6c5b
Yordan Stoykov <yordan.stoykov@modusbox.com>
2020-08-17 11:13:20 +0300

    Change loanAmount to reflect the outstanding amount due for the next scheduled installment that is unpaid

--- client-adapter/src/main/java/com/modusbox/client/router/PartiesRouter.java
+++ client-adapter/src/main/java/com/modusbox/client/router/PartiesRouter.java
@@ -28,27 +28,38 @@ public class PartiesRouter extends RouteBuilder {
 //					System.out.println();
 //				}
 //			})
+//			.process(exchange -> System.out.println())
 
 		// Add our global exception handling strategy
 		exceptionHandlingConfigurer.configureExceptionHandling(this);
 
 		// In this case the GET parties will return the loan account with client details
 		from("direct:getParties")
+			.routeId("getParties")
 			.log("Account lookup API called")
 			.log("GET on /${header.idType}/${header.idValue} called")
 			// Trim MFI code from id
 			.process(trimMFICode)
-			// First fetch the loan account by ID
+			// Fetch the loan account by ID so we can find customer ID
 			.to("direct:getLoanById")
-			// Now fetch the client information for the user the loan acc belongs to
+			// Fetch the loan schedule account by ID to find latest installment which is due
+			.to("direct:getLoanScheduleById")
+			// Now fetch the client information for the user the loan acc belongs to and get name
 			.to("direct:getClientById")
 			// Format the 2 responses
+			// Also save data as a header variable since the camel datasonnet processor imports JSON exchange properties only as string
+			.setHeader("getLoanByIdResponse", exchangeProperty("getLoanByIdResponse"))
+			.setHeader("getLoanScheduleByIdResponse", exchangeProperty("getLoanScheduleByIdResponse"))
+			.setHeader("getClientByIdResponse", exchangeProperty("getClientByIdResponse"))
 			.bean("getPartiesResponse")
+			.removeHeaders("getLoanByIdResponse")
+			.removeHeaders("getLoanScheduleByIdResponse")
+			.removeHeaders("getClientByIdResponse")
         ;
-
 
 		// API call to Mambu for loan information by ID
 		from("direct:getLoanById")
+			.routeId("getLoanById")
 			.log("Get loan account by ID route called")
 			.setBody(simple("{}"))
 
@@ -61,14 +72,33 @@ public class PartiesRouter extends RouteBuilder {
 			.toD("https://{{easy.mambu.host}}/loans/${header.idValueTrimmed}")
 
 			.unmarshal().json()
+			// Save response as property to use later
 			.setProperty("getLoanByIdResponse", body())
-			.setHeader("getLoanByIdResponse", body())
 //			.bean("getLoanByIdResponse")
 		;
 
+		// API call to Mambu for loan information by ID
+		from("direct:getLoanScheduleById")
+				.routeId("getLoanScheduleById")
+				.log("Get loan account schedule by ID route called")
+				.setBody(simple("{}"))
+
+				.removeHeaders("CamelHttp*")
+				.setHeader("Content-Type", constant("application/json"))
+				.setHeader("Accept", constant("application/vnd.mambu.v2+json"))
+				.setHeader(Exchange.HTTP_METHOD, constant("GET"))
+				.setProperty("authHeader", simple("${properties:easy.mambu.username}:${properties:easy.mambu.password}"))
+				.process(encodeAuthHeader)
+				.toD("https://{{easy.mambu.host}}/loans/${header.idValueTrimmed}/schedule")
+
+				.unmarshal().json()
+				// Save response as property to use later
+				.setProperty("getLoanScheduleByIdResponse", body())
+		;
 
 		// API call to Mambu for client information by ID
 		from("direct:getClientById")
+			.routeId("getClientById")
 			.log("Get client by ID route called")
 			.setBody(simple("{}"))
 
@@ -83,15 +113,13 @@ public class PartiesRouter extends RouteBuilder {
 			.unmarshal().json()
 			// Save response as property to use later
 			.setProperty("getClientByIdResponse", body())
-			// Also save it as a header variable since the camel datasonnet processor
-			// imports JSON exchange properties only as string
-			.setHeader("getClientByIdResponse", body())
 //			.bean("getClientByIdResponse")
 		;
 
-
+		/*
 		// API call to Mambu for fetching all loans for a client ID
 		from("direct:getLoansForClient")
+			.routeId("getLoansForClient")
 			.log("Get Loans for a client ID route called")
 			.setBody(simple("{}"))
 
@@ -104,5 +132,6 @@ public class PartiesRouter extends RouteBuilder {
 			.toD("https://{{easy.mambu.host}}/clients/${header.idValue}/loans")
 //			.bean("getLoansForClientResponse")
 		;
+		 */
 	}
 }
